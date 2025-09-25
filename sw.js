73739ccc9191a3a461fc63c9e7d778e0106433e5
juanrivero <juan.rivero@gmail.com>
2025-09-25 12:24:36 -0300

    Agregado de codigo para convertir en PWA

--- sw.js
+++ sw.js
@@ -1,23 +1,8 @@
-const CACHE_NAME = 'stickers-app-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+// sw.js
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('activate', (e) => {
+    e.waitUntil(self.clients.claim());
 });
